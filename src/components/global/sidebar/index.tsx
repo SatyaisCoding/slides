7b2a1d3cd86ef98089f3a9b06e96f0377bf26c1b
satya740 <satyaprakash74029@gmail.com>
2025-01-09 23:53:24 +0530

    sidebar-1/2

--- src/components/global/sidebar/index.tsx
+++ src/components/global/sidebar/index.tsx
@@ -1,14 +1,19 @@
-'use client'
-import React from 'react'
-import { Separator } from '@/components/ui/separator'
-
-
+"use client";
+import { usePaths } from "@/hooks/use-nav";
+import React from "react";
+import Items from "./items";
+import { Separator } from "@/components/ui/separator";
+import ClerkAuthState from "../clerk-auth-state";
+import { HelpDuoToneWhite } from "@/icons";
+import { SubscriptionPlan } from "../subscription-plan";
+import UpgradeCard from "./upgrade";
 
 type Props = {
-  slug: string
-}
+  slug: string;
+};
 
 const Sidebar = ({ slug }: Props) => {
+  const { page } = usePaths();
 
   return (
     <div
@@ -43,25 +48,32 @@ const Sidebar = ({ slug }: Props) => {
        backdrop-blur-3xl"
       >
         <div className="flex gap-x-2 items-center p-5 justify-center">
+          <LogoSmall />
         </div>
         <div className="flex flex-col py-3">
-        
+          <Items page={page} slug={slug} />
         </div>
         <div className="px-16">
-          <Separator
-            orientation="horizontal"
-            className="bg-[#333336]"
-          />
+          <Separator orientation="horizontal" className="bg-[#333336]" />
         </div>
         <div className="px-3 flex flex-col gap-y-5">
           <div className="flex gap-x-2">
+            <ClerkAuthState />
+            <p className="text-[#9B9CA0]">Profile</p>
+          </div>
+          <div className="flex gap-x-3">
+            <HelpDuoToneWhite />
             <p className="text-[#9B9CA0]">Help</p>
           </div>
         </div>
-   
+        <SubscriptionPlan type="FREE">
+          <div className="flex-1 flex flex-col justify-end">
+            <UpgradeCard />
+          </div>
+        </SubscriptionPlan>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
